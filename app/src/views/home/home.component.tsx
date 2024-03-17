399c96a702f1f6701d5fc584f1b11f8407f87984
GuilhemCouvin <guilhem.couvin@gmail.com>
2024-03-17 19:11:24 +0100

    clean repo

--- app/src/views/home/home.component.tsx
+++ app/src/views/home/home.component.tsx
@@ -2,9 +2,9 @@ import { Layout, Space } from "antd";
 import { HomeTitle } from "./components/home-title/home-title.component";
 import { UrlInput } from "./components/url-input/url-input.component";
 import { UrlSummary } from "./components/url-summary/url-summary.component";
-import "./home.style.css";
 import { UrlObject } from "../../types/url";
 import { useState } from "react";
+import "./home.style.css";
 
 export const Home = () => {
 	const [urlObjects, setUrlObjects] = useState<UrlObject[]>([]);
